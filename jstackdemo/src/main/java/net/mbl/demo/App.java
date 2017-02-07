da3e486828119f0755b5e538ad19742592bcc393
maobaolong <maobaolong@139.com>
2017-02-07 17:01:19 +0800

    - append demo of jstackdemo thread state in thread dump.

--- jstackdemo/src/main/java/net/mbl/demo/App.java
+++ jstackdemo/src/main/java/net/mbl/demo/App.java
@@ -35,7 +35,8 @@ public class App {
     t1.start();
     t2.start();
   }
-  private static void WAITING() {
+
+  private static void WAITING_FOR() {
     final Object lock = new Object();
     Thread t1 = new Thread(){
       @Override
@@ -56,9 +57,10 @@ public class App {
     Thread t2 = new Thread(){
       @Override
       public void run() {
+        int i= 0;
         while(true ){
           synchronized (lock) {
-            for(int i = 0; i< 10000000; i++){
+            for(i = 0; i< 10000000; i++){
               System. out.println(i);
             }
             lock.notifyAll();
@@ -70,13 +72,96 @@ public class App {
     t1.setName( "^^t1^^");
     t2.setName( "^^t2^^");
 
-    t1.start();
+
     t2.start();
+    t1.start();
+  }
+  private static void WAITING_SLEEP() {
+    final Object lock = new Object();
+    Thread t1 = new Thread(){
+      @Override
+      public void run() {
+        int i = 0;
+        while(true ){
+          synchronized (lock) {
+            try {
+              lock.wait();
+            } catch (InterruptedException e) {
+            }
+            System. out.println(i++);
+          }
+        }
+      }
+    };
+
+    Thread t2 = new Thread(){
+      @Override
+      public void run() {
+        int i= 0;
+        while(true ){
+          synchronized (lock) {
+            try {
+              sleep(30*1000L);
+            } catch (InterruptedException e) {
+            }
+            lock.notifyAll();
+          }
+        }
+      }
+    };
+
+    t1.setName( "^^t1^^");
+    t2.setName( "^^t2^^");
+
+
+    t2.start();
+    t1.start();
+  }
+
+  private static void TIMED_WAITING() {
+    final Object lock = new Object();
+    Thread t1 = new Thread(){
+      @Override
+      public void run() {
+        int i = 0;
+        while(true ){
+          synchronized (lock) {
+            try {
+              lock.wait(60 * 1000L);
+            } catch (InterruptedException e) {
+            }
+            System. out.println(i++);
+          }
+        }
+      }
+    };
+
+    Thread t2 = new Thread(){
+      @Override
+      public void run() {
+        int i= 0;
+        while(true ){
+          synchronized (lock) {
+            for(i = 0; i< 10000000; i++){
+              System. out.println(i);
+            }
+            lock.notifyAll();
+          }
+        }
+      }
+    };
+
+    t1.setName( "^^t1^^");
+    t2.setName( "^^t2^^");
+
+
+    t2.start();
+    t1.start();
   }
   public static void main(String[] args) {
     System.out.println("Hello World!");
 //    RUNNABLE();
 //    BLOCKED();
-    WAITING();
+    WAITING_SLEEP();
   }
 }
