321833cb00cbcf71ad52e011d6dff1a450fd9b36
maobaolong <maobaolong@139.com>
2017-07-28 23:38:22 +0800

    Something about lock downgrade.

--- concurrentdemo/src/main/java/net/mbl/demo/concurrentdemo/lock/rwlock/ClientRWLockDemo2.java
+++ concurrentdemo/src/main/java/net/mbl/demo/concurrentdemo/lock/rwlock/ClientRWLockDemo2.java
@@ -3,11 +3,14 @@ package net.mbl.demo.concurrentdemo.lock.rwlock;
 import java.util.concurrent.locks.Lock;
 
 public class ClientRWLockDemo2 {
+  //ClientRWLock unsupport reentrant, so unsupport lock downgrade too.
   private final static ClientRWLock clientRWLock = new ClientRWLock();
   public static void main(String[] args) {
-    Lock lock = clientRWLock.readLock();
-    lock.lock();
     clientRWLock.writeLock().lock();
+    clientRWLock.writeLock().lock();
+    Lock lock = clientRWLock.readLock();
+//    lock.lock();
+
     System.out.println("end");
 
   }
