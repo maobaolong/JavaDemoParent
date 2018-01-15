8e065920ab864e49ff22b8ec3a1f452a1fca5314
maobaolong <maobaolong@139.com>
2018-01-15 15:56:57 +0800

    jnidemo hold mem

--- jnidemo/src/main/c++/jnidemo.c
+++ jnidemo/src/main/c++/jnidemo.c
@@ -6,9 +6,10 @@ JNIEXPORT void Java_net_mbl_demo_jnidemo_JniDemo_sayHello(JNIEnv * env, jobject 
     printf("enter hello from jni\n");
     printf( "int = %d\n", len );
 
-    for (int i=0; i < len; i++) {
+    int i;
+    for (i=0; i < len; i++) {
         char* ptr = malloc(1 * 1024 * 1024);
-        memset(ptr, 0, sizeof(ptr));
+        memset(ptr, 0, 1 * 1024 * 1024);
     }
     printf("exit hello from jni\n");
 }
